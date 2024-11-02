76d3592e7332d1b23e169f947f4c067116cadd37
Vsevolod <158557439+Vsevolod-coding@users.noreply.github.com>
2024-11-02 13:37:43 +0400

    Update Repository.java (edited to T - Type)

--- Lesson_2/ClientServerApp/src/server/repository/Repository.java
+++ Lesson_2/ClientServerApp/src/server/repository/Repository.java
@@ -4,22 +4,22 @@ package server.repository;
  * The Repository interface defines the contract for a generic data storage mechanism.
  * It provides methods to save and load data as well as to check the existence of the storage file.
  *
- * @param <E> the type of data to be handled by the repository.
+ * @param <T> the type of data to be handled by the repository.
  */
-public interface Repository<E> {
+public interface Repository<T> {
     /**
      * Saves the provided data to the repository.
      *
      * @param data the data to be saved.
      */
-    void save(E data);
+    void save(T data);
 
     /**
      * Loads and returns the data from the repository.
      *
      * @return the loaded data of type E.
      */
-    E load();
+    T load();
 
     /**
      * Checks for the existence of the repository storage file and creates it if it does not exist.
